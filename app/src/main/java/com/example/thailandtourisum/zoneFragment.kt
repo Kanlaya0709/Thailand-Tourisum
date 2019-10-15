0ef037b87132c968a5dd745a3648aa58713f17e9
Kanlaya0709 <59160539@go.buu.ac.th>
2019-10-15 21:27:53 +0700

    initial project

---
+++ app/src/main/java/com/example/thailandtourisum/zoneFragment.kt
@@ -0,0 +1,30 @@
+package com.example.thailandtourisum
+
+
+import android.database.DatabaseUtils
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingComponent
+import androidx.databinding.DataBindingUtil
+import com.example.thailandtourisum.databinding.FragmenttZoneBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class zoneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentZoneBinding>(inflater
+            ,R.layout.fragment_zone,container,false)
+
+        return  binding.root
+
+    }
+
+
+}
