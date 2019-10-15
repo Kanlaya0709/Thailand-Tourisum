5d8a750f2a84e93accb87f0f89ce2b7901e8b481
Kanlaya0709 <59160539@go.buu.ac.th>
2019-10-16 00:50:01 +0700

    add custom_button

--- app/src/main/java/com/example/thailandtourisum/provinceFragment.kt
+++ app/src/main/java/com/example/thailandtourisum/provinceFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.thailandtourisum.databinding.FragmentProvinceBinding
 
 /**
@@ -20,6 +21,10 @@ class provinceFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentProvinceBinding>(inflater
             ,R.layout.fragment_province,container,false)
+
+        binding.button.setOnClickListener{ view ->
+            view.findNavController().navigate(R.id.action_provinceFragment_to_tourisumFragment)}
+
 
 
         return  binding.root
--- app/src/main/java/com/example/thailandtourisum/tourisumFragment.kt
+++ app/src/main/java/com/example/thailandtourisum/tourisumFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.thailandtourisum.databinding.FragmentTourisumBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +18,11 @@ class tourisumFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tourisum, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTourisumBinding>(inflater
+            ,R.layout.fragment_tourisum,container,false)
+
+
+        return  binding.root
     }
 
 
